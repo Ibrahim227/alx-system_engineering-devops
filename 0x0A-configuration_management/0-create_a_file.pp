4601b7c31ad037d036bbca2de4eac1572305afaa
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-22 09:44:19 -0700

    update 0-create_a_file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-$dir_root = "/tmp"
+$dir_root = '/tmp'
 
 file { $dir_root:
-    ensure => "directory",
-    mode => 0744,
-    path => "/tmp",
-    owner => "www-data",
-    group => "www-data",
-    content => "I love Puppet"
+    ensure  => 'directory',
+    mode    => '0744',
+    path    => '/tmp',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
 }
