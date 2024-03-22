7886bd580db2e9bfadca4f6e9b77a50007d0a27c
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-22 09:52:36 -0700

    update 0-create_a_file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 file { '/tmp/school':
     ensure  => 'school',
     mode    => '0744',
-    path    => '/tmp',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet'
