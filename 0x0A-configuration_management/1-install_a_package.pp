097f7d902fb01496e7d21b816c29ff57a10dcdec
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-22 16:57:11 -0700

    update 1-install_a_package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#Install a specific flask
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/bin',
 }
 
