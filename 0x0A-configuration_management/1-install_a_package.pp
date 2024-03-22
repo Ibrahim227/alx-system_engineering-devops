53e17e8eb1bea474a93b8bf63f3daa026deca083
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-22 10:15:48 -0700

    update 1-inistall_package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# Using Puppet, install flask and pip3
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path => '/usr/bin',
+  require => Package['python3-pip'],
+}
