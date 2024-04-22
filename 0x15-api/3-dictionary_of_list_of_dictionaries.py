ec013c09487acb7afbf9c40e1f9a1d984904ec3c
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-22 16:08:10 -0700

    update 3-dict_of_all_list_of_dictionnaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -52,13 +52,5 @@ def get_employee_progress(employee_id):
 
 
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    # Extract employee ID from command-line arguments
-    employee_id = int(sys.argv[1])
-
-    # Call the function to get and display TODO list progress
+    # Call the function
     get_employee_progress(employee_id)
