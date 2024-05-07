f5910890242d37e238323d3bdd26c58a7e49f3b1
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-07 10:25:28 -0700

    update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""Import required module/lib"""
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """queries Reddit API and returns the number of subscribers"""
-    if not subreddit:
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    else:
-        r = requests.get('https://www.reddit.com/dev/api#GET_api_v1_me')
-        return str(r)
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
