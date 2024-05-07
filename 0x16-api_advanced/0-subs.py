a44339e1f76724d023441dafa6becb4fc5ed82ca
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-07 10:01:25 -0700

    update 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Import required module/lib"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """queries Reddit API and returns the number of subscribers"""
+    if not subreddit:
+        return 0
+    else:
+        return subreddit
